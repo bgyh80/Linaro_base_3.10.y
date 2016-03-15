0f1bcafbbc1f4d92c0301a1e1486ca3f7495b461
Park Ju Hyung <qkrwngud825@gmail.com>
2016-03-16 05:37:55 +0900

    zpool, zsmalloc, zswap: merge with upstream Linux with strategic reverts mm/zbud.c - f58fb5e7f0ab05c9083869c1ec27854af2afc7b7 mm/zpool.c - 6f3526d6db7cbe8b53e42d6bf0cad2072afcf3fe mm/zsmalloc.c - c102f07ca0b04f2cb49cfc161c83f6239d17f491 mm/zswap.c - 8bc8b228d076ae93398316f81eab35f3d12c0c4f Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com> Signed-off-by: DQ <dqstyle@gmail.com>

--- include/linux/zsmalloc.h
+++ include/linux/zsmalloc.h
@@ -15,7 +15,6 @@
 #define _ZS_MALLOC_H_
 
 #include <linux/types.h>
-#include <linux/mm_types.h>
 
 /*
  * zsmalloc mapping modes
@@ -35,14 +34,14 @@ enum zs_mapmode {
 	 */
 };
 
-struct zs_ops {
-	struct page * (*alloc)(gfp_t);
-	void (*free)(struct page *);
+struct zs_pool_stats {
+	/* How many pages were migrated (freed) */
+	unsigned long pages_compacted;
 };
 
 struct zs_pool;
 
-struct zs_pool *zs_create_pool(gfp_t flags);
+struct zs_pool *zs_create_pool(const char *name, gfp_t flags);
 void zs_destroy_pool(struct zs_pool *pool);
 
 unsigned long zs_malloc(struct zs_pool *pool, size_t size);
@@ -53,5 +52,7 @@ void *zs_map_object(struct zs_pool *pool, unsigned long handle,
 void zs_unmap_object(struct zs_pool *pool, unsigned long handle);
 
 unsigned long zs_get_total_pages(struct zs_pool *pool);
+unsigned long zs_compact(struct zs_pool *pool);
 
+void zs_pool_stats(struct zs_pool *pool, struct zs_pool_stats *stats);
 #endif
